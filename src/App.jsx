888e3547c233d97ce1feb266e83a312c5ebc604b
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2022-12-06 09:00:10 +0000

    test nested router v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 import { About, Home, NotFound, Pricing, Admin, Auth } from "./pages";
 import "./app.css";
 import "./assets/css/utilities.css";
 import "./assets/css/typography.css";
 import "./assets/css/card.css";
+import { AdminPricing, Inquiry } from "./components";
+
+const router = createBrowserRouter([
+  {
+    path:"admin/",
+    element: <Admin/>,
+    children : [
+      {
+        path: "/admin/",
+        element: <Inquiry/>,
+      },
+      {
+        path: "/admin/pricing",
+        element: <AdminPricing/>
+      }
+    ]
+  },
+  {
+    path: '/',
+    element: <Home/>,
+  errorElement: <NotFound/>,
+
+  },
+  {
+    path: '/pricing',
+    element: <Pricing/>,
+  },{
+  },
+
+
+])
 
 const App = () => {
+
   return (
     <div className="app">
-      <BrowserRouter>
+      <RouterProvider router={router} />
+      {/* <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} index />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/about-us" element={<About />} />
           <Route path="/admin/*" element={<Admin />} />
           <Route path="/auth/*" element={<Auth />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" errorElement={<NotFound/>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 };
--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import DocumentTitle from "../../helpers/DocumentTitle";
 import "./notFound.css";
 
 const NotFound = () => {
   DocumentTitle("Page Not Found | Corebuso");
 
+  const error = useRouteError()
+
   return (
     <>
-      <div className="cbs__notfound _flex _flex-column  _jc-center _ai-center _h-100">
+      <div id="error-page" className="cbs__notfound _flex _flex-column  _jc-center _ai-center _h-100">
         <Link to="/" className="gradient__text">
           <h1>
             Home
           </h1>
             </Link>
         <h2 className="gradient__text">Oppss.. Page not found</h2>
+        <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
       </div>
 
     </>
