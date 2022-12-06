223014798ec75c21eed5717c12e9c2b0f76ae07e
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-12-06 19:01:34 +0800

    exclude dist folder in gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About, Home, NotFound, Pricing, Admin, Auth} from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { About, Home, NotFound, Pricing, Admin, Auth } from "./pages";
 import "./app.css";
 import "./assets/css/utilities.css";
 import "./assets/css/typography.css";
 import "./assets/css/card.css";
 import { Inquiry } from "./components";
 
-
 const App = () => {
-
   return (
     <div className="app">
-
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route exact path="/" element={<Home />}  errorElement={<NotFound/>} index />
+          <Route
+            exact
+            path="/"
+            element={<Home />}
+            errorElement={<NotFound />}
+            index
+          />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/about-us" element={<About />} />
           <Route path="/admin/*" element={<Admin />} />
           <Route path="/auth/*" element={<Auth />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
