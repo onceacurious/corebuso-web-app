156fbd6f18df6ec511038a17010a56ff7d6fa37e
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2022-12-12 01:22:59 +0800

    Update inquiryApi.js

--- src/helpers/api/inquiryApi.js
+++ src/helpers/api/inquiryApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const inquiryApi = axios.create({
-  baseURL: "http://127.0.0.1:8000/client/",
+  baseURL: "https://corebuso.pythonanywhere.com/client/",
 });
 
 export const getInquiry = async (id) => {
