2e26bdeda6ffffbbfbdd070579f05bc44df27faf
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-12-11 17:11:28 +0800

    nc

--- src/helpers/api/inquiryApi.js
+++ src/helpers/api/inquiryApi.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const inquiryApi = axios.create({
   baseURL: "https://corebuso.pythonanywhere.com/client/inquiry",
-  // baseURL: "http://127.0.0.1:8000/client/inquiry",
 });
 
 export const getInquiry = async (id) => {
