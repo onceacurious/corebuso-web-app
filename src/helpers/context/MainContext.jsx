fa2c26e9f2b065e3edb1c9e3d2dd9400ec17d010
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-12-13 20:28:39 +0800

    nc

--- src/helpers/context/MainContext.jsx
+++ src/helpers/context/MainContext.jsx
@@ -3,12 +3,18 @@ import { createContext, useState, useEffect } from "react";
 export const MainContext = createContext();
 
 export const MainProvider = ({ children }) => {
+  // Snackbar
   const [snackbarShow, setSnackbarShow] = useState(false);
   const [snackbarContent, setSnackbarContent] = useState(null);
   const [snackbarTitle, setSnackbarTitle] = useState(null);
   const [snackbarContextStatus, setSnackbarContextStatus] = useState("");
   const [snackbarDuration, setSnackbarDuration] = useState(5000);
+
+  // Pricing Modal
+  // TODO: Need to rename
   const [modalAction, setModalAction] = useState("");
+
+  // App
   const [scroll, setScroll] = useState(false);
 
   const setMainTimer = (time) => {};
